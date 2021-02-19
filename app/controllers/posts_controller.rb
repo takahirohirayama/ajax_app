6b11f0ef7736d56c6ac804c95db38c920d8e117c
takahirohirayama <honobonohinatabokko@gmail.com>
2021-02-20 07:46:44 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index  # indexアクションを定義した
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
+
   def checked
     post = Post.find(params[:id])
     if post.checked 
@@ -16,4 +18,5 @@ class PostsController < ApplicationController
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
 end
